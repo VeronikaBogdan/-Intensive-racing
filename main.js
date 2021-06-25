d7567646336e4c54ea55ad24358605d8aee7c727
Veronika <veron9399bog@gmail.com>
2021-06-25 14:23:16 +0300

    Add car traffic

--- main.js
+++ main.js
@@ -19,18 +19,33 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 3,
+    traffic: 3
 };
+
+function getQuantityElenents(heightElement) {
+    return document.documentElement.clientHeight / heightElement + 1;
+}
 
 function startGame() {
     start.classList.add('hide');
 
-    for (let i = 0; i < 20; i++){
+    for (let i = 0; i < getQuantityElenents(100); i++){
         const line = document.createElement('div');
         line.classList.add('line');
-        line.style.top = (i*75) + 'px';
-        line.y = i*75;
+        line.style.top = (i*100) + 'px';
+        line.y = i*100;
         gameArea.appendChild(line);
+    }
+
+    for (let i = 0; i < getQuantityElenents(100 * setting.traffic); i++){
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = -100 * setting.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url("./image/enemy.png") center / cover no-repeat';
+        gameArea.appendChild(enemy);
     }
 
     setting.start = true;
@@ -43,6 +58,7 @@ function startGame() {
 function playGame(){
     if (setting.start) {
         moveRoad();
+        moveEnemy();
         if (keys.ArrowLeft && setting.x > 3) {
             setting.x-= setting.speed; 
         }
@@ -79,7 +95,20 @@ function moveRoad() {
         line.style.top = line.y + 'px';
 
         if(line.y >= document.documentElement.clientHeight) {
-            line.y = -75;
+            line.y = -100;
         }
     });
 }
+
+function moveEnemy(){
+    let enemy = document.querySelectorAll('.enemy');
+    enemy.forEach(function(item){
+        item.y += setting.speed / 2;
+        item.style.top = item.y + 'px';
+
+        if (item.y >= document.documentElement.clientHeight){
+            item.y = -100 * setting.traffic;
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        }
+    });
+ }
